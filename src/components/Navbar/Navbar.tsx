47233ba6668cff8b162b8217baaf6816869c7906
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-19 13:10:56 -0700

    added semantic jsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,13 +4,17 @@ import Button from '../Button/Button'
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = () => (
-  <div className={styles.Navbar} data-testid="Navbar">
+  <nav className={styles.Navbar} data-testid="Navbar">
     <h1 className={styles.heading}>Scribble Notes</h1>
-    <div className='row'>
-      <Button title="Login" primary={true} />
-      <Button title="Sign Up" />
-    </div>
-  </div>
+    <ul className='row'>
+      <li>
+        <Button title="Login" primary={true} />
+      </li>
+      <li>
+        <Button title="Sign Up" />
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
