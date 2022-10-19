ccc7223a62e04286386c386315648bcfb6a50715
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-19 13:11:19 -0700

    added semantic jsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,8 +5,8 @@ interface ButtonProps {title:string, primary?:true}
 
 const Button: FC<ButtonProps> = ({title,primary}) => {
   return primary? 
-    (<div className={styles.Button} data-testid="Button">{title}</div>):
-    (<div className={styles.Secondary} data-testid="Button">{title}</div>)
+    (<a className={styles.Button} data-testid="Button">{title}</a>):
+    (<a className={styles.Secondary} data-testid="Button">{title}</a>)
 
 }
 
