511c7afd9cb9098de697a4ca58bcb197a9d01dd2
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-26 21:15:25 -0700

    created a pages folder

---
+++ src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+import styles from './LandingPage.module.css';
+import hero from '../../images/homepage.svg'
+import Button from '../../components/Button/Button'
+interface LandingPageProps {}
+
+const LandingPage: FC<LandingPageProps> = () => (
+  <div className={styles.LandingPage} data-testid="LandingPage">
+    <section>
+      <article className={styles.hero}>
+        <img src={hero} alt='hero character'></img>
+        <div className={styles.heroText}>
+          <h1>Welcome to Scribble&nbsp;Notes</h1>
+          <p>
+            This is the best place to track what you have to do. Just Scribble it down, and now you have it saved in one spot for later.
+          </p>
+          <div>
+            <Button title=' Click Here To Sign Up' />
+          </div>
+
+        </div>
+      </article>
+    </section>
+  </div>
+);
+
+export default LandingPage;
