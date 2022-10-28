ee6ee5a97ccbbe068647a37fd7512be9e3939169
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-27 20:26:42 -0700

    Turned into a layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Navbar from './components/Navbar/Navbar'
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <LandingPage />
+      <Outlet/>
     </div>
   );
 }
