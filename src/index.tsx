f981ccbe47de0a498cff28c8c546d4f8f9ce3808
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-27 20:27:05 -0700

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import LandingPage from './pages/LandingPage/LandingPage';
+import { createBrowserRouter, RouterProvider,Route, createRoutesFromElements } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import Login from './pages/Login/Login';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route>
+    <Route element={<App />}>
+      <Route path='/' element={<LandingPage />} />
+    </Route>
+    <Route path="/login" element={<Login/>}/>
+
+  </Route>
+  )
+  
+)
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
