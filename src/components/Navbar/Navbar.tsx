515106850e5ad8eb1b0e78ea33117d209745b102
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-10-31 14:48:49 -0700

    make brang name link to home page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,31 @@
 import React, { FC } from 'react';
 import styles from './Navbar.module.css';
 import Button from '../Button/Button'
+import {Link} from 'react-router-dom'
 interface NavbarProps {}
 
-const Navbar: FC<NavbarProps> = () => (
-  <nav className={styles.Navbar} data-testid="Navbar">
-    <h1 className={styles.heading}>Scribble Notes</h1>
-    <ul className='row'>
-      <li>
-          <Button link='/login' title="Login" primary={true} />
-      </li>
-      <li>
-        <Button link='/login' title="Sign Up" />
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar: FC<NavbarProps> = () => {
+  const linkStyle = {
+    textDecoration:"none",
+    color:"black",
+  }
+  return(
+    <nav className={styles.Navbar} data-testid="Navbar">
+      <h1 className={styles.heading}>
+      <Link style={linkStyle}to='/'>
+        Scribble Notes
+      </Link>
+      </h1>
+      <ul className='row'>
+        <li>
+            <Button link='/login' title="Login" primary={true} />
+        </li>
+        <li>
+          <Button link='/signup' title="Sign Up" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
