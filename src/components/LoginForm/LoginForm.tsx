9f648a108cfaeea9650a46aef58639fe533cf130
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-02 16:00:30 -0700

    added auto complete attribute

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -8,11 +8,11 @@ const LoginForm: FC<LoginFormProps> = () => (
     <form className={styles.Form}>
       <div className={styles.formElement}>
         <label htmlFor='username'>Username</label>
-        <input type="text" className={styles.Input} required name="username" placeholder="username" />
+        <input autoComplete='current-username' type="text" className={styles.Input} required name="username" placeholder="username" />
       </div>
       <div className={styles.formElement}>
         <label htmlFor='password'>Password</label>
-        <input type="password" className={styles.Input} required name="password" placeholder="password" />
+        <input autoComplete='current-password' type="password" className={styles.Input} required name="password" placeholder="password" />
       </div>
       <div className={styles.buttonContainer}>
         <button>Login</button>
