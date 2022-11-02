78dbe3b4956b1d3871d8d35c18a94380e528f142
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-11-02 16:01:20 -0700

    added switch and provider components

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,19 @@
 import React, { FC } from 'react';
 import styles from './LoginPage.module.css';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import FormSwitch from '../../components/FormSwitch/FormSwitch';
+import LoginWithProvider from '../../components/LoginWithProvider/LoginWithProvider';
 
 interface LoginPageProps {}
 
 const LoginPage: FC<LoginPageProps> = () => (
   <div className={styles.LoginPage} data-testid="LoginPage">
     <h1>Login</h1>
-    <LoginForm />
+    <div className={styles.LoginComponents}>
+      <LoginForm />
+      <FormSwitch link={'/signup'} title={'Switch To Sign Up'} />
+      <LoginWithProvider />
+    </div>
   </div>
 );
 
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from 'react';
 import styles from './Signup.module.css';
 import SignupForm from '../../components/SignupForm/SignupForm';
+import FormSwitch from '../../components/FormSwitch/FormSwitch';
+import LoginWithProvider from '../../components/LoginWithProvider/LoginWithProvider';
 interface LoginProps {}
 
 const Signup: FC<LoginProps> = () => (
   <div className={styles.Login} data-testid="Login">
       <h1>Sign Up</h1>
-      <SignupForm />
+      <div className={styles.LoginComponents}>
+        <SignupForm />
+        <FormSwitch title='Switch To Login' link='/login'/>
+        <LoginWithProvider />
+      </div>
   </div>
 );
 
